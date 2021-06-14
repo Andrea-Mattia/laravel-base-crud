144c6642d999e1b12128f4ea860acedadf6bfd44
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-06-14 17:57:41 +0200

    add: ComicController and index.blade.php

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main')
+
+@section('content')
+    <section class="container">
+        <h1 class="mb-5">Our Comics</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Title</td>
+                    <td>Series</td>
+                    <td colspan="3">Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <td>{{ $comic->id }}</td>
+                        <td>{{ $comic->title }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>SHOW</td>
+                        <td>EDIT</td>
+                        <td>DELETE</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
+@endsection
