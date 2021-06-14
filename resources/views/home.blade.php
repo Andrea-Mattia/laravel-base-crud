f2d56ddee967aa4f6290b17f57f7ddf75c876784
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-06-14 17:09:59 +0200

    add scaffolding and prepared PageController

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <section class="container">
+        <h1>Homepage here</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit quidem quos earum repudiandae atque eaque ducimus dolorum ipsam minima, qui laudantium! Ducimus dicta, laborum nam eveniet obcaecati maiores doloremque.</p>
+    </section>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="mt-5">
+    <p class="text-center">Andrea Mattia, Classe#30. All Rights Reserved &copy;</p>
+</footer>
